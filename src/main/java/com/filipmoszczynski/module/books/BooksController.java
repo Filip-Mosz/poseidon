feddfde9db99f86f35bb984ff7d889e71df721a0
Filip M <filip.moszczynski@onet.pl>
2020-10-17 15:38:09 +0200

    >Module 3.7 - Controller - DELETE

--- src/main/java/com/filipmoszczynski/module/books/BooksController.java
+++ src/main/java/com/filipmoszczynski/module/books/BooksController.java
@@ -29,4 +29,9 @@ public class BooksController {
     public BooksEntity newBooks(@RequestBody BooksEntity booksEntity) {
         return booksRepository.saveAndFlush(booksEntity);
     }
+
+    @DeleteMapping("/rest/books/{id}")
+    public void deleteBooks(@PathVariable Long id) {
+        booksRepository.deleteById(id);
+    }
 }
